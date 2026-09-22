523510a3263deb18da49829ca871c460d5b97a82
Sophia Denise Poldo <zoffh.xavier@gmail.com>
2026-09-22 16:01:02 +0800

    fix

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
+import './assets/customer-portal.css'
 import App from './App.vue'
 import router from './router'
 import Vue3Toastify from 'vue3-toastify'
